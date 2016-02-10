5a424bc3462c86e85951dac51ba53f0ffb70de7f
eriDam <erika.c.caballero@gmail.com>
2016-02-10 23:36:38 +0100

    Design UI, load in MainActivity

--- app/src/main/java/com/eridev/android/leefeeds/MainActivity.java
+++ app/src/main/java/com/eridev/android/leefeeds/MainActivity.java
@@ -1,13 +1,36 @@
 package com.eridev.android.leefeeds;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity implements View.OnClickListener {
+
+    Button boton1, boton2, boton3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initilize();
     }
+
+    private void initilize() {
+        boton1 = (Button) findViewById(R.id.button_1);
+        boton1.setOnClickListener(this);
+
+        boton2 = (Button) findViewById(R.id.button_2);
+        boton2.setOnClickListener(this);
+
+        boton3 = (Button) findViewById(R.id.button_3);
+        boton3.setOnClickListener(this);
+
+    }
+
+   
 }
